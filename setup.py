c34591c2dde6bdca685f5868dd00cd68a19ec312
Roy Hooper <rhooper@toybox.ca>
2013-11-01 18:08:22 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oauth',
-    version='0.0.2',
+    version='0.0.3',
     description='OAuth Signature Generation/Validation',
     author='Naitik Shah',
     author_email='n@daaku.org',
